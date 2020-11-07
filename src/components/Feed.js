5578366ba91b20cb65e23442fdc5d8cc0ec78106
jacek zablocki <56847035+actionjacks@users.noreply.github.com>
2020-11-07 22:41:01 +0100

    add animation flipmove

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -3,6 +3,7 @@ import "../styles/Feed.css";
 import Post from "./Post";
 import TweetBox from "./TweetBox";
 import db from "../firebase";
+import FlipMove from "react-flip-move";
 
 function Feed() {
   const [posts, setPosts] = useState([]);
@@ -19,16 +20,19 @@ function Feed() {
         <h2>feed</h2>
       </div>
       <TweetBox />
-      {posts?.map((post) => (
-        <Post
-          displayName={post.displayName}
-          username={post.username}
-          verified={post.verified}
-          text={post.text}
-          image={post.image}
-          avatar={post.avatar}
-        />
-      ))}
+      <FlipMove>
+        {posts?.map((post) => (
+          <Post
+            key={post.text}
+            displayName={post.displayName}
+            username={post.username}
+            verified={post.verified}
+            text={post.text}
+            image={post.image}
+            avatar={post.avatar}
+          />
+        ))}
+      </FlipMove>
     </div>
   );
 }
--- src/components/Post.js
+++ src/components/Post.js
@@ -1,5 +1,5 @@
 import { Avatar } from "@material-ui/core";
-import React from "react";
+import React, { forwardRef } from "react";
 import "../styles/Post.css";
 import VerifiedUserIcon from "@material-ui/icons/VerifiedUser";
 import ChatBubbleOutlineIcon from "@material-ui/icons/ChatBubbleOutline";
@@ -7,37 +7,39 @@ import RepeatIcon from "@material-ui/icons/Repeat";
 import FavoriteBorderIcon from "@material-ui/icons/FavoriteBorder";
 import PublishIcon from "@material-ui/icons/Publish";
 
-function Post({ displayName, username, verified, text, image, avatar }) {
-  return (
-    <div className="post">
-      <div className="post__avatar">
-        <Avatar src={avatar} />
-      </div>
-      <div className="post__body">
-        <div className="post__header">
-          <div className="post__headerText">
-            <h3>
-              {displayName}
-              <span>
-                {verified && <VerifiedUserIcon className="post__badge" />}
-                {username}
-              </span>
-            </h3>
+const Post = forwardRef(
+  ({ displayName, username, verified, text, image, avatar }, ref) => {
+    return (
+      <div className="post" ref={ref}>
+        <div className="post__avatar">
+          <Avatar src={avatar} />
+        </div>
+        <div className="post__body">
+          <div className="post__header">
+            <div className="post__headerText">
+              <h3>
+                {displayName}
+                <span>
+                  {verified && <VerifiedUserIcon className="post__badge" />}
+                  {username}
+                </span>
+              </h3>
+            </div>
+            <div className="post__headerDescription">
+              <p>{text}</p>
+            </div>
           </div>
-          <div className="post__headerDescription">
-            <p>{text}</p>
+          <img src={image} alt="" />
+          <div className="post__footer">
+            <ChatBubbleOutlineIcon fontSize="small" />
+            <RepeatIcon fontSize="small" />
+            <FavoriteBorderIcon fontSize="small" />
+            <PublishIcon fontSize="small" />
           </div>
         </div>
-        <img src={image} alt="" />
-        <div className="post__footer">
-          <ChatBubbleOutlineIcon fontSize="small" />
-          <RepeatIcon fontSize="small" />
-          <FavoriteBorderIcon fontSize="small" />
-          <PublishIcon fontSize="small" />
-        </div>
       </div>
-    </div>
-  );
-}
+    );
+  }
+);
 
 export default Post;
